24fdc96ef0b700a971254049d69b9345d76f07f1
chiagbanweghi101 <c.peter@alustudent.com>
2024-10-25 05:42:51 -0700

    add script to update run

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 'a' <= char <= 'z':  # Check if the character is lowercase
+            result += chr(ord(char) - 32)  # Convert to uppercase
+        else:
+            result += char  # Keep the character as is
+    print(result)  # Print the result
+
