8e4d72de4105806ee152b2bc3ac1bae6c84b4568
chiagbanweghi101 <c.peter@alustudent.com>
2025-02-06 12:06:30 +0000

    update all script

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+const oldSquare = require('./5-square');
+class Square extends oldSquare {
   charPrint (c) {
     if (c === undefined) {
       this.print();
     } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
+      for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.height));
       }
     }
   }
-};
+}
+module.exports = Square;
